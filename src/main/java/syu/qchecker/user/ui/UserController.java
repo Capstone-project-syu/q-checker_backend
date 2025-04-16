b53cc10499574edb6a76684b4e0621a630def6e2
leeshin20 <leeshin20@naver.com>
2025-04-16 20:51:27 +0900

    feat: SwaggerConfig 추가

--- src/main/java/syu/qchecker/user/ui/UserController.java
+++ src/main/java/syu/qchecker/user/ui/UserController.java
@@ -1,4 +1,17 @@
 package syu.qchecker.user.ui;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "User API", description = "User 관련 API")
 public class UserController {
+
+    @Operation(summary = "예시 API", description = "예시 API입니다.")
+    @GetMapping("/users/{id}")
+    public String getUser() {
+        return "유저 정보";
+    }
 }
