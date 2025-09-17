bd2a4019463a345f9bdc61f02d74e54c60c13dd8
leeshin20 <leeshin20@naver.com>
2025-09-17 17:00:00 +0900

    feat: UserDto에 이메일 인증 관련 정보 추가

--- src/main/java/syu/qchecker/user/dto/UserRequestDto.java
+++ src/main/java/syu/qchecker/user/dto/UserRequestDto.java
@@ -1,14 +1,33 @@
 package syu.qchecker.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "사용자 등록/수정 요청 DTO")
 public class UserRequestDto {
+    
+    @Schema(description = "소셜 로그인 타입", example = "google")
     private String socialType;
+    
+    @Schema(description = "소셜 계정 ID")
     private String socialId;
+    
+    @Schema(description = "소셜 로그인 이메일", example = "user@gmail.com")
     private String email;
+    
+    @Schema(description = "사용자 이름", example = "홍길동")
     private String name;
+    
+    @Schema(description = "학번", example = "20210001")
     private Long studentNumber;
+    
+    @Schema(description = "인증된 대학교 이메일", example = "student@syu.ac.kr")
+    private String universityEmail;
 } 
